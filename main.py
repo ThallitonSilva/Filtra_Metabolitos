bb69ca37058795c8014e5d10457f153d9db55b9f
Thalliton Silva <66505881+ThallitonSilva@users.noreply.github.com>
2024-04-11 15:29:41 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,62 @@ import pandas as pd
 import streamlit as st
 import io
 
-pd.set_option("display.precision", 19)
+pd.set_option("display.precision", 20)
+pd.set_option('display.float_format', '{:.20f}'.format)
 
 
-def make_excel(table):
+def make_excel(tb1, tb2, tb3, tb4):
     buffer = io.BytesIO()
 
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-        table.to_excel(writer, index=False)
+        tb1.to_excel(writer, sheet_name=f'Raw_Data', index=False)
+        tb2.to_excel(writer, sheet_name=f'Filter_QueryMass', index=False)
+        tb3.to_excel(writer, sheet_name=f'Filter_MatchedCompound', index=False)
+        tb4.to_excel(writer, sheet_name=f'Final_Result', index=False)
 
     return buffer
 
 
+def filtro1(tabela):
+    gp = tabela.groupby('Query.Mass')
+
+    dff = pd.DataFrame(columns=['ID', 'Query.Mass', 'Matched.Compound', 'Matched.Form', 'Mass.Diff'])
+
+    for nome, tab in gp:
+        minimo = tab['Mass.Diff'].min()
+
+        df_temp = tab[tab['Mass.Diff'] == minimo]
+
+        dff = pd.merge(dff, df_temp, how='outer')
+
+    return dff
+
+
+def filtro2(tabela):
+    gp = tabela.groupby('Matched.Compound')
+
+    dff = pd.DataFrame(columns=['Query.Mass', 'Matched.Compound', 'Matched.Form', 'Mass.Diff'])
+
+    for nome, tab in gp:
+        minimo = tab['Mass.Diff'].min()
+
+        df_temp = tab[tab['Mass.Diff'] == minimo]
+
+        dff = pd.merge(dff, df_temp, how='outer')
+
+    dff.sort_values('Query.Mass', inplace=True)
+
+    return dff
+
+
+def filtro3(tabela):
+    dff = tabela.drop_duplicates('Query.Mass', keep=False)
+
+    return dff
+
+
 # --- Page Config
-st.set_page_config(page_title='Filtrar Metabolitos', layout='wide')
+st.set_page_config(page_title='Horario dos Funcionários', layout='wide')
 
 hide_menu_style = """
         <style>
@@ -30,28 +72,18 @@ st.title('Filtrar os Metabólitos')
 arquivo = st.file_uploader('Insira a tabela dos Metabólitos', accept_multiple_files=False)
 
 if arquivo:
-    tabela = pd.read_excel(arquivo)
-    tabela['Query.Mass'] = tabela['Query.Mass'].map(lambda x: '{0:.6f}'.format(x))
-    tabela.insert(0, 'ID', range(len(tabela)))
+    to_filter = pd.read_excel(arquivo)
+    to_filter['Query.Mass'] = to_filter['Query.Mass'].map(lambda x: '{0:.6f}'.format(x))
 
-    gp = tabela.groupby('Query.Mass')
+    df = filtro1(to_filter)
+    df2 = filtro2(df)
+    df3 = filtro3(df2)
 
-    df = pd.DataFrame(columns=['ID', 'Query.Mass', 'Matched.Compound', 'Matched.Form', 'Mass.Diff'])
+    st.write(df3)
 
-    for nome, tab in gp:
-        minimo = tab['Mass.Diff'].min()
-
-        df_temp = tab[tab['Mass.Diff'] == minimo]
-
-        df = pd.merge(df, df_temp, how='outer')
-
-    df = df.sort_values('ID').reset_index(drop=True)
-    df = df.drop(columns='ID')
-    st.dataframe(df)
-
-    excel_gliri = make_excel(df)
+    excel = make_excel(to_filter, df, df2, df3)
 
     st.download_button(label=f'Download dos metabólitos filtrados',
-                       data=excel_gliri,
+                       data=excel,
                        file_name=f'Metabólitos_Filtrados.xlsx',
                        mime="application/vnd.ms-excel")
